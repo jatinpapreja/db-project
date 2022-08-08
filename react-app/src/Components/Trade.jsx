35d15034ee58d3f60c0395d13ca7d3ff17d7ba48
GudiyaPilare <87381022+GudiyaPilare@users.noreply.github.com>
2022-08-08 18:10:53 +0530

    Update Trade.jsx

--- react-app/src/Components/Trade.jsx
+++ react-app/src/Components/Trade.jsx
@@ -157,9 +157,12 @@ function Trade(props) {
   return (
     <div>
       <NavbarTrade name="User Name" />
-      <div className="ag-theme-alpine" style={{height:1000}}>
+      <div className="ag-theme-alpine" style={{height:600}}>
         <AgGridReact rowData={securities} columnDefs={trade_heading} 
-        defaultColDef={defaultColDef} />
+        defaultColDef={defaultColDef} 
+        pagination={true}
+        paginationPageSize={10}
+        paginationAutoPageSize={true}/>
       </div>
 
     {/* <div style={containerStyle}>
