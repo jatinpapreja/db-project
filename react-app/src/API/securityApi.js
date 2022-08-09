96b1db073bc44bcedd679b72fd4c64087622b191
Devansh Suthar <61281698+dss-0620@users.noreply.github.com>
2022-08-10 00:29:26 +0530

    Delete Security Bug Fixed

--- react-app/src/API/securityApi.js
+++ react-app/src/API/securityApi.js
@@ -18,6 +18,7 @@ export async function getSecurities() {
 
 export async function deleteSecurity(securityId) {
     const response = await server.delete(`api/v1/Security/${securityId}`);
+    location.reload();
    //  return response.data;
 }
 
